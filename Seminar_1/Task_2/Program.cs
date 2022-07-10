9aa5cde585d768ad57c8d74c8bb420a9abe25e58
Kirill <kirilllatks@gmail.com>
2022-07-10 18:36:35 +0300

    Сделал и протестил вторую программу

--- Seminar_1/Task_2/Program.cs
+++ Seminar_1/Task_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Программа определяет какое число из трёх больше. Введи первое число:       ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число:      ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи третье число:      ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number1 > number2) { max = number1; } else {max = number2;}
+if (max > number3) { max = max; } else {max = number3;}
+{
+    Console.Write($"Наибольшее из трёх чисел:        {max}");
+}
