15eb3048fdfbdc01159998da260a1492106ec6db
Kirill <kirilllatks@gmail.com>
2022-07-10 18:22:59 +0300

    Сделал первую задачу

--- Seminar_1/Task_1/Program.cs
+++ Seminar_1/Task_1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Программа определяет какое число из двух больше. Введи первое число:       ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number1 > number2)
+{
+    Console.Write($"Наибольшее из двух чисел:        {number1}");
+}
+else
+{
+    Console.Write($"Наибольшее из двух чисел:        {number2}");
+}
