ada51bdb3783a46e79d4c2d6a44a500f11407588
Kirill <kirilllatks@gmail.com>
2022-07-16 15:43:23 +0300

    Дз к 2 семинару

---
+++ Seminar_1/Task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+string number = Console.ReadLine();
+if (number.Length > 2)
+{
+    Console.WriteLine(number[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+};
---
+++ Seminar_1/Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число дня недели.");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number > 5 && number < 8)
+{
+    Console.WriteLine("Выходной");
+}
+else if (number > 0 && number < 6)
+{
+    Console.WriteLine("Рабочий");
+}
+else 
+{
+    Console.WriteLine("Число вне диапазона недели");
+};
