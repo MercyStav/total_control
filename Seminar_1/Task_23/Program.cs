5b6d80ffd0bc1a1591ae42a5d4028bffeb03b9f9
Kirill <kirilllatks@gmail.com>
2022-07-22 18:47:11 +0300

    Таблица кубов

---
+++ Seminar_1/Task_23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число    ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine("Таблица кубов чисел:");
+while (i <= n){
+    Console.WriteLine($"| {i} | {i * i * i}|");
+    i++ ;
+};
