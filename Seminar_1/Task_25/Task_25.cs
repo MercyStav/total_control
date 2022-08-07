c80669a0ea0a721c3dfd9892be54b03d960ffe30
Kirill <kirilllatks@gmail.com>
2022-08-07 12:44:30 +0300

    считаем натуральную степень

---
+++ Seminar_1/Task_25/Task_25.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число A:      ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи число B:      ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = A;
+for (int i = 2; i <= B; i++)
+{
+    A = A * C;
+}
+Console.Write($"{A}");
