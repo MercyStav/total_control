cedf4316a1fc203cf257a95c4bd7354066d28e29
Kirill <kirilllatks@gmail.com>
2022-08-16 21:16:35 +0300

    сумма цифр числа

---
+++ Seminar_1/Task_27/Task_27.cs
@@ -0,0 +1,10 @@
+﻿ int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (num > 0)
+            {
+ 
+                sum = sum + num % 10;
+                num = num /10 ;
+ 
+            }
+ Console.WriteLine(sum);
