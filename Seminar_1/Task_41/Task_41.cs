676b8c2aa8b113ba8aee3b80520d666d5f4906c4
Kirill <kirilllatks@gmail.com>
2022-08-21 21:31:49 +0300

    Задача 41

---
+++ Seminar_1/Task_41/Task_41.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Программа определяет колличество введённых чисел больше 0, введи сколько ты хочешь проверить чисел:       ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [M];
+for (int i = 0; i < M; i++)
+{
+    Console.Write("Вводи число и жми enter:      ");
+    int j = Convert.ToInt32(Console.ReadLine());
+    array[i] = j;
+}
+int res = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) res++;
+};
+Console.Write($"Ты ввёл {res} чисел больше 0");
