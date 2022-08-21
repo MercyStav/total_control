c7b11431c4fd5e857cbf304c5f4d3c5e0b74a9da
Kirill <kirilllatks@gmail.com>
2022-08-21 20:46:37 +0300

    Задача 38

---
+++ Seminar_1/Task_38/Task_38.cs
@@ -0,0 +1,9 @@
+﻿double[] mass = { 21.2800, 13.09, 5.55, 55.123, 23.234234 };
+double max = 0;
+double min = 100;
+for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] > max) max = mass[i];
+        if (mass[i] < min) min = mass[i];
+    }
+Console.WriteLine($"Разница между максимальными и минимальными значениями массива составляет: {max - min}");
