6b2fb7f9dfaa4d67f4915abff5061d1fcdf15831
Kirill <kirilllatks@gmail.com>
2022-08-21 20:08:10 +0300

    Задача 34

---
+++ Seminar_1/Task_34/Task_34.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int [40];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,999);
+    Console.Write($"{array[i]} ");
+}
+int res = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0) res++;
+}
+Console.WriteLine($"Колличество чётных элементов в массиве: {res}");
