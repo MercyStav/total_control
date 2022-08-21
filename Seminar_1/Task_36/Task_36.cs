5cb7a734c5ca946a0766d797dbaadfae4a9f44ed
Kirill <kirilllatks@gmail.com>
2022-08-21 20:18:14 +0300

    Задача 36

---
+++ Seminar_1/Task_36/Task_36.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int [10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write($"{array[i]} ");
+}
+int res = 0;
+for (int j = 1 ; j < array.Length; j = j + 2)
+{
+    res = res + array[j];
+}
+Console.WriteLine($"Сумма элементов стоящих на нечётных позициях массива: {res}");
