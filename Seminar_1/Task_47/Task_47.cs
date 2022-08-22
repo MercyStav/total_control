0643a91b89e0ce7359d1c395a61a08ec6af587c7
Kirill <kirilllatks@gmail.com>
2022-08-22 23:28:44 +0300

    Задача 47

---
+++ Seminar_1/Task_47/Task_47.cs
@@ -0,0 +1,12 @@
+﻿double[,] a = new double[4, 4];
+
+Random random = new Random();
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        a[i, j] = random.NextDouble() * 50;
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
