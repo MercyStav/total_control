18b3465395adb813d0c28a1e100749501fd841d9
Kirill <kirilllatks@gmail.com>
2022-08-24 00:07:35 +0300

    Задача 52

---
+++ Seminar_1/Task_52/Task_52.cs
@@ -0,0 +1,21 @@
+﻿int[,] mass = new int[7, 7];
+Random ran = new Random();
+for (int i = 0; i < 7; i++)
+{
+    for (int j = 0; j < 7; j++)
+    {
+       mass[i, j] = ran.Next(1, 15);
+       Console.Write("{0}\t", mass[i, j]);
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < mass.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        sum += mass[i, j];
+    }
+    double average = sum / mass.GetLength(0);
+    Console.WriteLine($"Среднее арифметическое столбца {j} = {average:F2}");
+}
