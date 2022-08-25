4feef366fa7ade9b0d1e47691fa66d5bcc6bde77
Kirill <kirilllatks@gmail.com>
2022-08-26 00:06:52 +0300

    Задача 54

---
+++ Seminar_1/Task_54/Task_54.cs
@@ -0,0 +1,46 @@
+﻿int[,] mass = new int[10, 10];
+Console.WriteLine($"\nНеотсортированный массив: ");
+CreateArray(mass);
+WriteArray(mass);
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(mass);
+WriteArray(mass);
+void OrderArrayLines(int[,] mass)
+{
+  for (int i = 0; i < mass.GetLength(0); i++)
+  {
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+      for (int k = 0; k < mass.GetLength(1) - 1; k++)
+      {
+        if (mass[i, k] < mass[i, k + 1])
+        {
+          int temp = mass[i, k + 1];
+          mass[i, k + 1] = mass[i, k];
+          mass[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+void CreateArray(int[,] mass)
+{
+  for (int i = 0; i < mass.GetLength(0); i++)
+  {
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+      mass[i, j] = new Random().Next(1, 15);
+    }
+  }
+}
+void WriteArray(int[,] mass)
+{
+  for (int i = 0; i < mass.GetLength(0); i++)
+  {
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+      Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
