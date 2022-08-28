57b36f09f37fb4624b686c9b48902bd3a67762e9
Kirill <kirilllatks@gmail.com>
2022-08-28 20:56:03 +0300

    Задача 66

---
+++ Seminar_1/Task_66/Task_66.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int m = InputNumbers("Введи первое число: ");
+int n = InputNumbers("Введи второе число: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма натуральных чисел = {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+} 
