38f2992d6e94e31e87c1fe441dff10cddab875b1
Kirill <kirilllatks@gmail.com>
2022-08-28 14:47:03 +0300

    Задача 58

---
+++ Seminar_1/Task_58/Task_58.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+int[,] firstMartrix = new int[7, 7];
+CreateMass(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteMass(firstMartrix);
+
+int[,] twoMartrix = new int[7, 7];
+CreateMass(twoMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteMass(twoMartrix);
+
+int[,] resultMatrix = new int[7,7];
+
+MultiplyMatrix(firstMartrix, twoMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteMass(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] twoMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * twoMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+void CreateMass(int[,] Mass)
+{
+  for (int i = 0; i < Mass.GetLength(0); i++)
+  {
+    for (int j = 0; j < Mass.GetLength(1); j++)
+    {
+      Mass[i, j] = new Random().Next(1, 15);
+    }
+  }
+}
+
+void WriteMass (int[,] Mass)
+{
+  for (int i = 0; i < Mass.GetLength(0); i++)
+  {
+    for (int j = 0; j < Mass.GetLength(1); j++)
+    {
+      Console.Write(Mass[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
