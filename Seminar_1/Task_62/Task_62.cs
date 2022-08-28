da93961c9658eb1ed92a9769091751636dbe2a18
Kirill <kirilllatks@gmail.com>
2022-08-28 15:34:50 +0300

    Задача 62

---
+++ Seminar_1/Task_62/Task_62.cs
@@ -0,0 +1,36 @@
+﻿int[,] newMass = new int[7, 7];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= newMass.GetLength(0) * newMass.GetLength(1))
+{
+  newMass[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < newMass.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= newMass.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > newMass.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteMass(newMass);
+
+void WriteMass (int[,] mass)
+{
+  for (int i = 0; i < mass.GetLength(0); i++)
+  {
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+      if (mass[i,j] / 10 <= 0)
+      Console.Write($" {mass[i,j]} ");
+
+      else Console.Write($"{mass[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
